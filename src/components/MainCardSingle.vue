841cd025b2a92b7a61cc59c5cba9af3fc0000aee
Jimmy Shura <jshura@stu.southtexascollege.edu>
2025-04-20 08:35:30 -0500

    card details finished

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -32,7 +32,7 @@ const props = defineProps({
     <!-- {{ fullname }} <br /> -->
     <RouterLink :to="{name: 'CardDetail', params: {id: props.employee._id}}">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
-            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height: 480, width: 960})" />
+            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cat', height: 480, width: 960})" />
             <div class="p-4">
                 <p class="mb-2 text-2xl font-bold">{{ props.employee.firstName }} {{ props.employee.lastName }}</p>
                 <p class="mb-3 font-normal text-gray-800">{{ props.employee.title }}</p>
